e03521a06a4652980782bcb184362f1df0072b56
Ballester <ballester.inbox@gmail.com>
2024-02-27 02:44:01 -0500

    Update marketplace link to PROS

--- docs/components/Ad.jsx
+++ docs/components/Ad.jsx
@@ -2,13 +2,13 @@ import styles from "./Ad.module.css";
 
 export default () => (
   <a
-    href="https://marketplace.everymundo.com/"
+    href="https://marketplace.pros.com/"
     target="_black"
     className={styles.panel}
   >
     <div className={styles.innerPanel}>
       Discover the world's top offer marketing technology
-      <span className={styles.markeplace}>EveryMundo Marketplace</span>
+      <span className={styles.markeplace}>PROS Marketplace</span>
     </div>
   </a>
 );
