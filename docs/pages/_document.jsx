47b60044bb054f35a26ca305e9c835637b8d014a
Ballester <ballester.inbox@gmail.com>
2024-02-27 02:33:13 -0500

    Update links from Everymundo/registry to Everymundo/developer-hub

--- docs/pages/_document.jsx
+++ docs/pages/_document.jsx
@@ -6,47 +6,47 @@ class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="https://everymundo.github.io/registry/docs/favicon-32x32.png" />
+          <link rel="icon" href="https://everymundo.github.io/developer-hub/docs/favicon-32x32.png" />
           <meta name="theme-color" content="#f38020" />
           <link
             rel="apple-touch-icon"
             sizes="48x48"
-            href="https://everymundo.github.io/registry/docs/icons/icon-48x48.png"
+            href="https://everymundo.github.io/developer-hub/docs/icons/icon-48x48.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="72x72"
-            href="https://everymundo.github.io/registry/docs/icons/icon-72x72.png"
+            href="https://everymundo.github.io/developer-hub/docs/icons/icon-72x72.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="96x96"
-            href="https://everymundo.github.io/registry/docs/icons/icon-96x96.png"
+            href="https://everymundo.github.io/developer-hub/docs/icons/icon-96x96.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="144x144"
-            href="https://everymundo.github.io/registry/docs/icons/icon-144x144.png"
+            href="https://everymundo.github.io/developer-hub/docs/icons/icon-144x144.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="192x192"
-            href="https://everymundo.github.io/registry/docs/icons/icon-192x192.png"
+            href="https://everymundo.github.io/developer-hub/docs/icons/icon-192x192.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="256x256"
-            href="https://everymundo.github.io/registry/docs/icons/icon-256x256.png"
+            href="https://everymundo.github.io/developer-hub/docs/icons/icon-256x256.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="384x384"
-            href="https://everymundo.github.io/registry/docs/icons/icon-384x384.png"
+            href="https://everymundo.github.io/developer-hub/docs/icons/icon-384x384.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="512x512"
-            href="https://everymundo.github.io/registry/docs/icons/icon-512x512.png"
+            href="https://everymundo.github.io/developer-hub/docs/icons/icon-512x512.png"
           />
         </Head>
         <body>
